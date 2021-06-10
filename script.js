4bb3078f66d99efa46564a5fd589f7f991dbae7e
Danhalf <danhel@rocketmail.com>
2021-06-10 15:38:03 +0300

    update 2

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
-let first;
+const API_KEY =
+  'pk.eyJ1IjoiZGFuaGFsZiIsImEiOiJja3A2dzhnOHAwdW93MndxdHVsa3dxOXFpIn0.or1NJxIWSuvz3VPQjPXhmA';
 
-fetch(
-  'https://api.mapbox.com/directions-matrix/v1/mapbox/driving/30.160912,50.326342;30.974,49.915?access_token=pk.eyJ1IjoiZGFuaGFsZiIsImEiOiJja3A2dzhnOHAwdW93MndxdHVsa3dxOXFpIn0.or1NJxIWSuvz3VPQjPXhmA'
-)
-  .then((res) => res.json())
-  .then((res) => console.log(res));
+mapboxgl.accessToken = API_KEY;
+const map = new mapboxgl.Map({
+  container: 'map', // container id
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [30.50912, 50.326342],
+  zoom: 8.5,
+});
+map.scrollZoom.disable();
+const [lng, lan] = [30.95553, 50.34845];
+
+const from = [30.160912, 50.326342]; //lng, lat
+const to = [lng, lan]; //lng, lat
+
+const greenMarker = new mapboxgl.Marker({
+  color: '#ffde3b',
+})
+  .setLngLat(to) // marker position using variable 'to'
+  .addTo(map); //add marker to map
+
+const purpleMarker = new mapboxgl.Marker({
+  color: '#007336',
+})
+  .setLngLat(from) // marker position using variable 'from'
+  .addTo(map); //add marker to map
+
+const options = {
+  units: 'kilometers',
+}; // units can be degrees, radians, miles, or kilometers, just be sure to change the units in the text box to match.
+
+const distance = turf.distance(to, from, options);
+
+const value = document.getElementById('map-overlay');
+value.innerHTML = `Відстань: ${distance.toFixed([
+  2,
+])} кілометрів, орієнтована вартість доставки: ${(distance * 22).toFixed([
+  2,
+])} грн`;
