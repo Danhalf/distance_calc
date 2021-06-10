ffa1de683a50eea81f821e35ae9dfb0b4332af3a
Danhalf <danhel@rocketmail.com>
2021-06-10 16:43:00 +0300

    update 4

--- script.js
+++ script.js
@@ -9,12 +9,18 @@ const map = new mapboxgl.Map({
   zoom: 8.5,
 });
 map.scrollZoom.disable();
-const [lng, lan] = [30.95553, 50.34845];
+
+const inputValue = document.querySelector('#calc-region');
+const coordinates = inputValue.value.split`,`.map((e) => +e);
+console.log(coordinates);
+const [lng, lan] = coordinates;
 
 const from = [30.160912, 50.326342]; //lng, lat
-const to = [lng, lan]; //lng, lat
+let to = [lng, lan]; //lng, lat
 
-const greenMarker = new mapboxgl.Marker({
+const pricePerKm = 22;
+
+let markerTo = new mapboxgl.Marker({
   color: '#ffde3b',
 })
   .setLngLat(to) // marker position using variable 'to'
@@ -26,6 +32,16 @@ const purpleMarker = new mapboxgl.Marker({
   .setLngLat(from) // marker position using variable 'from'
   .addTo(map); //add marker to map
 
+inputValue.addEventListener('change', () => {
+  console.log(inputValue.value);
+  markerTo.remove();
+  markerTo = new mapboxgl.Marker({
+    color: '#ffde3b',
+  })
+    .setLngLat(to)
+    .addTo(map); //add marker to map; // marker position using variable 'to'
+});
+
 const options = {
   units: 'kilometers',
 }; // units can be degrees, radians, miles, or kilometers, just be sure to change the units in the text box to match.
@@ -35,6 +51,6 @@ const distance = turf.distance(to, from, options);
 const value = document.getElementById('map-overlay');
 value.innerHTML = `Відстань: ${distance.toFixed([
   2,
-])} кілометрів, орієнтована вартість доставки: ${(distance * 22).toFixed([
-  2,
-])} грн`;
+])} кілометрів, орієнтована вартість доставки: ${(
+  distance * pricePerKm
+).toFixed([2])} грн`;
